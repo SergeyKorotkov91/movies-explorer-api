9f253027c5419e221f240c9e3107abae6f20078d
SergeyKorotkov91 <121323544+SergeyKorotkov91@users.noreply.github.com>
2023-09-26 01:30:14 +0300

    Update error.js

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,6 @@
 const error = (err, req, res, next) => {
   const { statusCode = 500, message } = err;
+  console.error(err);
 
   res.status(statusCode).send({ message: statusCode === 500 ? 'Ошибка сервера' : message });
   next();
